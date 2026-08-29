e6e738b865006075130abf6d562f4dd5afbcebbf
prjdee <probohomo@gmail.com>
2026-08-29 14:49:27 +0200

    Add initial SoundCloud fan reviews dataset

---
+++ js/reviews-data.js
@@ -0,0 +1,201 @@
+// Automatically curated fan & DJ reviews from SoundCloud
+const communityReviews = [
+    {
+        "id": 1,
+        "author": "Jesus B.",
+        "username": "Jesus B.",
+        "avatar": "https://i1.sndcdn.com/avatars-HvY1PeUnyayIAfQR-89BHxw-large.jpg",
+        "comment": "This sounds like a charming gallazokoop",
+        "trackTitle": "Cats - Project Dee (Original Mix)",
+        "trackUrl": "https://soundcloud.com/project_d2025/cats",
+        "trackThumb": "https://i1.sndcdn.com/artworks-UYe3tOWaT3u8nSoV-7Bx6pw-t500x500.jpg",
+        "date": "2026-08-29"
+    },
+    {
+        "id": 2,
+        "author": "Suzy Davies",
+        "username": "Suzy Davies",
+        "avatar": "https://i1.sndcdn.com/avatars-000245858999-hme53b-large.jpg",
+        "comment": "Delightful cat sounds. Imaginative track.",
+        "trackTitle": "Cats - Project Dee (Original Mix)",
+        "trackUrl": "https://soundcloud.com/project_d2025/cats",
+        "trackThumb": "https://i1.sndcdn.com/artworks-UYe3tOWaT3u8nSoV-7Bx6pw-t500x500.jpg",
+        "date": "2026-08-28"
+    },
+    {
+        "id": 3,
+        "author": "Twistedreload",
+        "username": "Twistedreload",
+        "avatar": "https://i1.sndcdn.com/avatars-zQyC3JGbznKEvqzz-0KNqRQ-large.jpg",
+        "comment": "clean mix",
+        "trackTitle": "Cats - Project Dee (Original Mix)",
+        "trackUrl": "https://soundcloud.com/project_d2025/cats",
+        "trackThumb": "https://i1.sndcdn.com/artworks-UYe3tOWaT3u8nSoV-7Bx6pw-t500x500.jpg",
+        "date": "2026-08-28"
+    },
+    {
+        "id": 4,
+        "author": "Zawley",
+        "username": "Zawley",
+        "avatar": "https://i1.sndcdn.com/avatars-xpQM6UizvtqEGJ0J-Uj0Yug-large.jpg",
+        "comment": "Vibes, straught vibes. Thats all I gotta say🔥🕺",
+        "trackTitle": "Cats - Project Dee (Original Mix)",
+        "trackUrl": "https://soundcloud.com/project_d2025/cats",
+        "trackThumb": "https://i1.sndcdn.com/artworks-UYe3tOWaT3u8nSoV-7Bx6pw-t500x500.jpg",
+        "date": "2026-08-28"
+    },
+    {
+        "id": 5,
+        "author": "farbod barikani",
+        "username": "Farbod",
+        "avatar": "https://i1.sndcdn.com/avatars-yPdnAB7cFRlhvBzA-cTC76A-large.jpg",
+        "comment": "បាយាដ និសត កេ បេយសដ ាសដលកឹិ ាសដ្ស ឹុិសុហថ ាលសក្ដ លា",
+        "trackTitle": "Cats - Project Dee (Original Mix)",
+        "trackUrl": "https://soundcloud.com/project_d2025/cats",
+        "trackThumb": "https://i1.sndcdn.com/artworks-UYe3tOWaT3u8nSoV-7Bx6pw-t500x500.jpg",
+        "date": "2026-08-27"
+    },
+    {
+        "id": 6,
+        "author": "Michael Meyer",
+        "username": "DJ Mandala",
+        "avatar": "https://i1.sndcdn.com/avatars-t8eFjyERzFXiqzvq-H8LwJA-large.jpg",
+        "comment": "on point",
+        "trackTitle": "Cats - Project Dee (Original Mix)",
+        "trackUrl": "https://soundcloud.com/project_d2025/cats",
+        "trackThumb": "https://i1.sndcdn.com/artworks-UYe3tOWaT3u8nSoV-7Bx6pw-t500x500.jpg",
+        "date": "2026-08-27"
+    },
+    {
+        "id": 7,
+        "author": "Sergio Guarneri",
+        "username": "Sergio Guarneri",
+        "avatar": "https://i1.sndcdn.com/avatars-figE8AR2MjoMwAbj-PH6ihA-large.jpg",
+        "comment": "beautiful 👍",
+        "trackTitle": "Cats - Project Dee (Original Mix)",
+        "trackUrl": "https://soundcloud.com/project_d2025/cats",
+        "trackThumb": "https://i1.sndcdn.com/artworks-UYe3tOWaT3u8nSoV-7Bx6pw-t500x500.jpg",
+        "date": "2026-08-27"
+    },
+    {
+        "id": 8,
+        "author": "nz44au",
+        "username": "nz44au",
+        "avatar": "https://i1.sndcdn.com/avatars-y8rXXSB3zN1yNX1k-OC4G2g-large.jpg",
+        "comment": "nice one like the athmosphere",
+        "trackTitle": "Cats - Project Dee (Original Mix)",
+        "trackUrl": "https://soundcloud.com/project_d2025/cats",
+        "trackThumb": "https://i1.sndcdn.com/artworks-UYe3tOWaT3u8nSoV-7Bx6pw-t500x500.jpg",
+        "date": "2026-08-27"
+    },
+    {
+        "id": 9,
+        "author": "???",
+        "username": "Mr.8",
+        "avatar": "https://i1.sndcdn.com/avatars-CGHmAmOrGEBEAphO-fxszPw-large.jpg",
+        "comment": "nice song, cool voice, nice cats!",
+        "trackTitle": "Cats - Project Dee (Original Mix)",
+        "trackUrl": "https://soundcloud.com/project_d2025/cats",
+        "trackThumb": "https://i1.sndcdn.com/artworks-UYe3tOWaT3u8nSoV-7Bx6pw-t500x500.jpg",
+        "date": "2026-08-27"
+    },
+    {
+        "id": 10,
+        "author": "Tyler Harris",
+        "username": "Baby Junegem",
+        "avatar": "https://i1.sndcdn.com/avatars-9UtOP4QNyuRUtyU2-S5pwXg-large.jpg",
+        "comment": "Ts really cool gng I fck wit it heavy",
+        "trackTitle": "Cats - Project Dee (Original Mix)",
+        "trackUrl": "https://soundcloud.com/project_d2025/cats",
+        "trackThumb": "https://i1.sndcdn.com/artworks-UYe3tOWaT3u8nSoV-7Bx6pw-t500x500.jpg",
+        "date": "2026-08-27"
+    },
+    {
+        "id": 11,
+        "author": "Balm Angel",
+        "username": "Balm Angel",
+        "avatar": "https://i1.sndcdn.com/avatars-001379431920-epwwj0-large.jpg",
+        "comment": "doing fine with this record to be honest",
+        "trackTitle": "Cats - Project Dee (Original Mix)",
+        "trackUrl": "https://soundcloud.com/project_d2025/cats",
+        "trackThumb": "https://i1.sndcdn.com/artworks-UYe3tOWaT3u8nSoV-7Bx6pw-t500x500.jpg",
+        "date": "2026-08-26"
+    },
+    {
+        "id": 12,
+        "author": "Record Label",
+        "username": "Quintessential Records",
+        "avatar": "https://i1.sndcdn.com/avatars-TinHNlPtg5Ge4Z2H-nYh02w-large.jpg",
+        "comment": "What a unique and different concept for a dance track!",
+        "trackTitle": "Cats - Project Dee (Original Mix)",
+        "trackUrl": "https://soundcloud.com/project_d2025/cats",
+        "trackThumb": "https://i1.sndcdn.com/artworks-UYe3tOWaT3u8nSoV-7Bx6pw-t500x500.jpg",
+        "date": "2026-08-26"
+    },
+    {
+        "id": 13,
+        "author": "SKLA",
+        "username": "SKLA",
+        "avatar": "https://i1.sndcdn.com/avatars-RyYSjqzkfHNPYQVw-KZF0NQ-large.jpg",
+        "comment": "this is a crazy concept of a track, nice work",
+        "trackTitle": "Cats - Project Dee (Original Mix)",
+        "trackUrl": "https://soundcloud.com/project_d2025/cats",
+        "trackThumb": "https://i1.sndcdn.com/artworks-UYe3tOWaT3u8nSoV-7Bx6pw-t500x500.jpg",
+        "date": "2026-08-26"
+    },
+    {
+        "id": 14,
+        "author": "Sativadiva",
+        "username": "Sativadiva",
+        "avatar": "https://i1.sndcdn.com/avatars-ffqSvOAyxReUQPe4-CzoZaA-large.jpg",
+        "comment": "boss vibes 👏",
+        "trackTitle": "Cats - Project Dee (Original Mix)",
+        "trackUrl": "https://soundcloud.com/project_d2025/cats",
+        "trackThumb": "https://i1.sndcdn.com/artworks-UYe3tOWaT3u8nSoV-7Bx6pw-t500x500.jpg",
+        "date": "2026-08-26"
+    },
+    {
+        "id": 15,
+        "author": "Yasin Borry",
+        "username": "BYAS",
+        "avatar": "https://i1.sndcdn.com/avatars-BzpeLbRvfBK33R9Q-QcpK4w-large.jpg",
+        "comment": "Cool synth layering and drum groove",
+        "trackTitle": "Cats - Project Dee (Original Mix)",
+        "trackUrl": "https://soundcloud.com/project_d2025/cats",
+        "trackThumb": "https://i1.sndcdn.com/artworks-UYe3tOWaT3u8nSoV-7Bx6pw-t500x500.jpg",
+        "date": "2026-08-26"
+    },
+    {
+        "id": 16,
+        "author": "Marcio Reis",
+        "username": "Reeis",
+        "avatar": "https://i1.sndcdn.com/avatars-mESAMtnB20DGKhO3-B4VEvw-large.jpg",
+        "comment": "Produção muito interessante , bem pop e radiofonia!",
+        "trackTitle": "Cats - Project Dee (Original Mix)",
+        "trackUrl": "https://soundcloud.com/project_d2025/cats",
+        "trackThumb": "https://i1.sndcdn.com/artworks-UYe3tOWaT3u8nSoV-7Bx6pw-t500x500.jpg",
+        "date": "2026-08-26"
+    },
+    {
+        "id": 17,
+        "author": "Matteo Girardi",
+        "username": "Mathew Turns",
+        "avatar": "https://a1.sndcdn.com/images/default_avatar_large.png",
+        "comment": "wow!That voice! Cool 😎",
+        "trackTitle": "Cats - Project Dee (Original Mix)",
+        "trackUrl": "https://soundcloud.com/project_d2025/cats",
+        "trackThumb": "https://i1.sndcdn.com/artworks-UYe3tOWaT3u8nSoV-7Bx6pw-t500x500.jpg",
+        "date": "2026-08-26"
+    },
+    {
+        "id": 18,
+        "author": "Dudi Levin",
+        "username": "Sweet Solution",
+        "avatar": "https://i1.sndcdn.com/avatars-fNrreloO8JVAMtVJ-vcRjBA-large.jpg",
+        "comment": "nice track and vibe .... good mixed",
+        "trackTitle": "Cats - Project Dee (Original Mix)",
+        "trackUrl": "https://soundcloud.com/project_d2025/cats",
+        "trackThumb": "https://i1.sndcdn.com/artworks-UYe3tOWaT3u8nSoV-7Bx6pw-t500x500.jpg",
+        "date": "2026-08-26"
+    }
+];
